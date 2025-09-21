8875a765f86827d0d631101f5169e00be7b80f1b
leolilaia <laialeonardo09@gmail.com>
2025-09-21 17:44:24 -0300

    Add files via upload

---
+++ Módulo 2/aula2_questao1.py
@@ -0,0 +1,9 @@
+num1 = 5
+num2 = 2
+print(num1)
+print(type(num1))
+print(num2)
+print(type(num2))
+num3 = num1/num2
+print(num3)
+print(type(num3))
---
+++ Módulo 2/aula2_questao2.py
@@ -0,0 +1,12 @@
+resultado = "o resultado é"
+num1 = 10
+num2 = 3.5
+num3 = num1 + num2
+print(resultado)
+print(type(resultado))
+print(num1)
+print(type(num1))
+print(num2)
+print((type(num2)))
+print(num3)
+print((type(num3)))
