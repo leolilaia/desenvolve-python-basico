049bb1bd27dd14c8b151a4e6c8132b8b628b9612
leolilaia <laialeonardo09@gmail.com>
2025-09-23 23:05:08 -0300

    Delete Módulo 3/Aula 3/aula2_questao2.py

--- Módulo 3/Aula 3/aula2_questao2.py
+++
@@ -1,9 +0,0 @@
-#Solicita a idade de Cris e Juliana
-Cris = int(input("Digite a idade de Cris: "))
-Juliana = int(input("Digite a idade de Juliana: "))
-
-#Verifica a idade de ambos(desta vez utilizando o "or" no lugar do "and")
-Permitido = Juliana > 17 or Cris > 17
-
-#imprime o resultado na tela
-print(Permitido)
