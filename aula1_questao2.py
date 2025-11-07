d9f8acc1e1c2a3d8772f62ff6719735d4564f2e6
leolilaia <laialeonardo09@gmail.com>
2025-11-07 14:35:06 -0300

    Delete aula1_questao2.py

--- aula1_questao2.py
+++
@@ -1,9 +0,0 @@
-# Solicita nome e sobrenome
-primeiro_nome = input("Digite seu primeiro nome: ")
-sobrenome = input("Digite seu sobrenome: ")
-
-# Concatena as strings
-nome_completo = primeiro_nome + " " + sobrenome
-
-# Exibe a mensagem final
-print(f"Bem-vinda, {nome_completo}!")
