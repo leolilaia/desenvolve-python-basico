9b8e54c88d5134a09ba2fd50133e2bef4aec6b09
leolilaia <laialeonardo09@gmail.com>
2025-11-07 14:32:44 -0300

    Delete Módulo 7 directory

--- Módulo 7/aula1_questao3.py
+++
@@ -1,8 +0,0 @@
-# Solicita a frase ao usuário
-frase = input("Digite uma frase: ")
-
-# Conta quantos espaços em branco tem na frase
-espacos = frase.count(" ")
-
-# Exibe o resultado
-print("Espaços em branco: ", espacos)
